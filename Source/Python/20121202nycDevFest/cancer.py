ea8e3213056a1e6856af895e937c0c541b51551f
mduby <mduby@broadinstitute.org>
2017-12-02 10:19:16 -0500

    added dev fest nyc cancer ML talk

---
+++ Source/Python/20121202nycDevFest/cancer.py
@@ -0,0 +1,23 @@
+
+
+from tensorflow inport keras
+
+model = keras.models.Sequential()
+model.add(layers.Conv2D(32, (3, 3), activation = = 'relu')
+          
+          
+
+
+
+
+from keras.applications import InceptionV3
+vonv_base = InceptionV3(weights = 'imagenet', include_top = false, input_shape = )
+
+
+
+
+from keras import models
+from keras import layers
+
+
+
--- Source/Python/Hail/20171010restSetup/DCC/dccGenericHailRestServer.py
+++ Source/Python/Hail/20171010restSetup/DCC/dccGenericHailRestServer.py
@@ -1,19 +1,32 @@
 
-import os, sys
 # import os, sys, IPython - from Ryan@UM
+import os, sys
 from os import path
 from glob import glob
 from pprint import pprint
 
+# set the constants
 # set up Hail home
-#  entries, one for the packaged distribution that works, another for the REST branch compiled distribution that doesn't
-# packaged Hail
-# HAIL_HOME = "/home/ubuntu/hail_server/distribHail"
 # REST branch compiled hail
 HAIL_HOME = "/home/ubuntu/hail_server/jonHailTree20171114"
+# set up SPARK home
+SPARK_HOME = "/home/ubuntu/Software/spark-2.0.2-bin-hadoop2.7"
+# set the REST server port
+SERVER_PORT = 6064
+pprint("server port: " + str(SERVER_PORT))
+# set the log file path
+LOG_FILE_PATH = "/home/ubuntu/Logs/HailServers/hail" + str(SERVER_PORT) + ".log"
+pprint("log file: " + LOG_FILE_PATH)
+# set the VCF path
+VCF_PATH = "/home/ubuntu/Data/Hail/Biome/biome_illumina.vcf"
+pprint("vcf file: " + VCF_PATH)
+# set the phenotype file path and sample key
+PHENOTYPE_FILE_PATH = "/home/ubuntu/Data/Hail/Biome/biome_illumina.ped"
+pprint("ped file: " + PHENOTYPE_FILE_PATH)
+SAMPLE_KEY = "ID"
+
 
 # Setup HAIL and PYTHONPATH
-SPARK_HOME = "/home/ubuntu/Software/spark-2.0.2-bin-hadoop2.7"
 PY4J_ZIPS = glob(path.join(SPARK_HOME,"python/lib/py4j-*-src.zip"))
 PYTHON_LIBS = [
   "{}/python".format(HAIL_HOME),
@@ -23,12 +36,6 @@ PYTHONPATH = ":".join(PYTHON_LIBS)
 sys.path = PYTHON_LIBS + sys.path
 
 # set the Spark classpath; one line each for distribution Hail and one for Jon's branch of the code
-# for distribution of Hail
-# SPARK_CLASSPATH = "{}/jars/hail-all-spark.jar".format(HAIL_HOME)
-# for REST code branch
-# SPARK_CLASSPATH = "{}/build/install/hail/lib/*".format(HAIL_HOME)
-# Iteration 1 - old Jon path that didn't work 
-# SPARK_CLASSPATH = "{}/build/libs/hail-all-spark.jar".format(HAIL_HOME)
 SPARK_CLASSPATH = "{}/build/install/hail/lib/*".format(HAIL_HOME)
 print SPARK_CLASSPATH
 
@@ -46,23 +53,21 @@ from hail import *
 
 
 # create the Hail context
-hc = HailContext(log='/home/ubuntu/Logs/HailServers/hail6063.log')
+hc = HailContext(log=LOG_FILE_PATH)
 
 
 # import the CVF
-vcf_path = "/home/ubuntu/Data/Hail/Camp/camp.biallelic.chr1-22.clean.vcf"
-campVds = hc.import_vcf(vcf_path)
+vds = hc.import_vcf(VCF_PATH)
 
 # load in phenotypes
 # use impute = True to let hail determine the type of the columns
-ped_path = "/home/ubuntu/Data/Hail/Camp/camp.phenotypes.epacts.withId.modified.ped"
-phenotypes = hc.import_table(ped_path, impute=True).key_by("ID")
+phenotypes = hc.import_table(PHENOTYPE_FILE_PATH, impute=True).key_by(SAMPLE_KEY)
 pprint(phenotypes)
 
 
 # annotate the loaded vds
-campVdsAnnotated = campVds.annotate_samples_table(phenotypes, root="sa")
-pprint(campVdsAnnotated.sample_schema)
+vdsAnnotated = vds.annotate_samples_table(phenotypes, root="sa")
+pprint(vdsAnnotated.sample_schema)
 
 
 # load covariates
@@ -75,7 +80,7 @@ pprint(covariates)
 
 
 # start the server
-campVdsAnnotated.rest_server_linreg(covariates, False, 6063)
+vdsAnnotated.rest_server_linreg(covariates, True, SERVER_PORT)
 
 
 
