0bba848a31bbcc34a976122fdccf84471b4ed17f
mduby <mduby@broadinstitute.org>
2017-12-23 16:13:22 -0500

    added start TF code

---
+++ Source/Python/TensorFlow/CodeLabRetrain/virtualEnvSetup.sh
@@ -0,0 +1,57 @@
+
+
+conda install virtualenv
+which virtualenv
+python
+virtualenv --system-site-packages ~/Apps/PythonEnvs/TensorFlow01
+source ~/Apps/PythonEnvs/TensorFlow01/bin/activate
+which pip
+easy_install -U pip
+pip -version
+pip -V
+pip install --upgrade tensorflow
+
+# clone TF
+git clone https://github.com/googlecodelabs/tensorflow-for-poets-2 TensorFlowPoets02
+
+# get the model to work on
+curl http://download.tensorflow.org/example_images/flower_photos.tgz | tar xz -C tf_files
+
+# set the model for retraining (224px image size, 1/2 of mobile net )
+IMAGE_SIZE=224
+ARCHITECTURE="mobilenet_0.50_${IMAGE_SIZE}"
+
+# launch tensorblard monitoring tool
+tensorboard --logdir tf_files/training_summaries &
+# to clean, do below
+# pkill -f "tensorboard"
+
+# read help on the retrain script
+python -m scripts.retrain -h
+
+
+# retrain command
+python -m scripts.retrain \
+  --bottleneck_dir=tf_files/bottlenecks \
+  --how_many_training_steps=500 \
+  --model_dir=tf_files/models/ \
+  --summaries_dir=tf_files/training_summaries/"${ARCHITECTURE}" \
+  --output_graph=tf_files/retrained_graph.pb \
+  --output_labels=tf_files/retrained_labels.txt \
+  --architecture="${ARCHITECTURE}" \
+  --image_dir=tf_files/flower_photos
+
+
+# help for the label image script
+python -m  scripts.label_image -h
+
+# run the labe script
+python -m scripts.label_image \
+    --graph=tf_files/retrained_graph.pb  \
+    --image=tf_files/flower_photos/daisy/21652746_cc379e0eea_m.jpg
+
+python -m scripts.label_image \
+    --graph=tf_files/retrained_graph.pb  \
+    --image=tf_files/flower_photos/roses/2414954629_3708a1a04d.jpg
+
+
