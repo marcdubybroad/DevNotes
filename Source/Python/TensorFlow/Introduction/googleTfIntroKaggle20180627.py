728f765e59a493dff42fd8754b65cfc59f874bb7
mduby <mduby@broadinstitute.org>
2018-06-27 10:43:42 -0400

    adding ML train code lab

---
+++ Source/Python/TensorFlow/Introduction/googleTfIntroKaggle20180627.py
@@ -0,0 +1,61 @@
+
+
+
+# This Python 3 environment comes with many helpful analytics libraries installed
+# It is defined by the kaggle/python docker image: https://github.com/kaggle/docker-python
+# For example, here's several helpful packages to load in 
+
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+
+# Input data files are available in the "../input/" directory.
+# For example, running this (by clicking run or pressing Shift+Enter) will list the files in the input directory
+
+import os
+# print(os.listdir("../input"))
+
+# Any results you write to the current directory are saved as output.
+import itertools
+import os
+
+%matplotlib inline
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+
+from sklearn.preprocessing import LabelBinarizer, LabelEncoder
+from sklearn.metrics import confusion_matrix
+
+from tensorflow import keras
+layers = keras.layers
+models = keras.models
+
+
+# This code was tested with TensorFlow v1.8
+print("You have TensorFlow version", tf.__version__)
+
+
+# load data
+# cell 2
+data = pd.read_csv("../input/bbc-text.csv")
+
+data.head()
+
+data['category'].value_counts()
+
+# split data into traint/test
+train_size = int(len(data) * .8)
+print ("Train size: %d" % train_size)
+print ("Test size: %d" % (len(data) - train_size))
+
+def train_test_split(data, train_size):
+    train = data[:train_size]
+    test = data[train_size:]
+    return train, test
+
+
+
+
+
+
