e41693a337b9b7c1a1b403cc1de9a95dea15be80
mduby <mduby@broadinstitute.org>
2018-07-14 13:31:58 -0400

    adedd pythin test code

--- Source/Python/IntroductionPython/Exercises/exercise02cDataStructures.py
+++ Source/Python/IntroductionPython/Exercises/exercise02cDataStructures.py
@@ -36,3 +36,64 @@ eclipse_dates = ['June 21, 2001', 'December 4, 2002', 'November 23, 2003',
 third_to_end_index = len(eclipse_dates) - 3
 print(eclipse_dates[third_to_end_index:])
 print(eclipse_dates[-3:])
+eclipse_dates.append('July 6, 2018')
+
+
+# sets
+a = [1, 2, 2, 3, 3, 3, 4, 4, 4, 4]
+b = set(a)
+print(len(a) - len(b))
+
+b.add(5)
+b.pop()
+
+# Define a Dictionary, population,
+# that provides information
+# on the world's largest cities.
+# The key is the name of a city
+# (a string), and the associated
+# value is its population in
+# millions of people.
+
+#   Key     |   Value
+# Shanghai  |   17.8
+# Istanbul  |   13.3
+# Karachi   |   13.0
+# Mumbai    |   12.5
+population = {'Shanghai': 17.8, 'Istanbul': 13.3, 'Karachi': 13.0, 'Mumbai': 12.5}
+print(population)
+
+
+# is and ==
+a = [1, 2, 3]
+b = a
+c = [1, 2, 3]
+
+print(a == b)
+print(a is b)
+print(a == c)
+print(a is c)
+
+# quiz 01
+animals = {'dogs': [20, 10, 15, 8, 32, 15], 
+ 'cats': [3,4,2,8,2,4], 
+ 'rabbits': [2, 3, 3], 
+ 'fish': [0.3, 0.5, 0.8, 0.3, 1]}
+ 
+print(animals['dogs'])
+print(animals['dogs'][3])
+
+
+# 2d dictionaries
+elements = {'hydrogen': {'number': 1, 'weight': 1.00794, 'symbol': 'H'},
+            'helium': {'number': 2, 'weight': 4.002602, 'symbol': 'He'}}
+
+
+elements['hydrogen']['is_noble_gas'] = False
+elements['helium']['is_noble_gas'] = True
+
+print(elements['hydrogen']['is_noble_gas'])
+print(elements['helium']['is_noble_gas'])
+
+
+
---
+++ Source/Python/IntroductionPython/Scratch/mlBookSnippets.py
@@ -0,0 +1,33 @@
+
+# while loop
+count = 0
+while (count < 3) :
+    print(count, " is less than 3")
+    print count, " is less than 3"
+    count = count + 1
+else :
+    print(count, " is not less than 3")
+    
+# lists
+var3 = [1, 2, 3]
+var3 = [1, 2, 3]
+print(var3 is var3)
+
+var3 = [1, 2, 3]
+var4 = [1, 2, 3]
+print(var3 is var4)
+
+
+# sets
+seta = {1, 2, 3, 4, 5}
+setb = {3, 4, 5, 6, 7}
+set_diff = seta.difference(setb)
+print(set_diff)
+
+
+
+
+
+
+
+
