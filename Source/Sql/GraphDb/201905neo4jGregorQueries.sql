3397d04478709b8b6a8eb0957a3b2900904733e4
mduby <mduby@broadinstitute.org>
2019-05-31 09:40:26 -0400

    Graph: adding random query

--- Source/Sql/GraphDb/201905neo4jGregorQueries.sql
+++ Source/Sql/GraphDb/201905neo4jGregorQueries.sql
@@ -3,20 +3,60 @@
 match 
   (pheno : Phenotype {name: 'T2D'})-[link :HAS_GLOBAL_ENRICHMENT]->(enrich :GlobalEnrichment),
   (enrich)<-[:HAS_GLOBAL_ENRICHMENT]-(tissue :Tissue),
-  (enrich)<-[:HAS_GLOBAL_ENRICHMENT]-(ancestry :Ancestry),
-  (enrich)-[:HAS_ANNOTATION]->(annot :Annotation)
+  (enrich)<-[:HAS_GLOBAL_ENRICHMENT]-(ancestry :Ancestry)
 return pheno.name as phenotype, tissue.name as tissue, ancestry.name as ancestry,
     enrich.pValue as p_value, enrich.inBedIndexSNP as in_beb_index_snp, enrich.expectedInBedIndexSNP as expected_in_beb_index_snp,
-    annot.name as annotation
+    enrich.annotation as annotation
 order by enrich.pValue
+limit 50
 
-limit 10
+
+-- object query
+match (pheno : Phenotype {name: 'T2D'})-[link :HAS_GLOBAL_ENRICHMENT]->(enrich :GlobalEnrichment),
+    (enrich)<-[link2 :HAS_GLOBAL_ENRICHMENT]-(tissue :Tissue),
+    (enrich)<-[:HAS_GLOBAL_ENRICHMENT]-(ancestry :Ancestry)
+return pheno, tissue, ancestry, enrich, link, link2
+limit 50
+
+order by enrich.pValue
 
 
 -- meta analysis for single variant
 match (phenotype: Phenotype)-[vm:HAS_GLOBAL_ENRICHMENT]->(meta: MetaAnalysis)<-[pm:HAS_META_ANALYSIS]-(pheno: Phenotype)
 where var.position = 118232242 and var.chromosome = '8'
 return var, meta, pheno, vm, pm
+
+
+
+
+
+-- random query
+match (v:Variant {name: '16:53803574:T:A'})
+
+// find regions that overlap the variant
+match (o:OverlappedRegion)
+where o.chromosome = v.chromosome and o.start <= v.position
+with * order by o.start desc limit 1
+match (r:Region)-[:OVERLAPS]-(o)
+where r.start <= v.position and r.end > v.position
+with r, v
+
+// find globally enriched tissues that these regions affect
+match (t:Tissue)-[:HAS_REGION]->(r),
+      (t)-[:HAS_GLOBAL_ENRICHMENT]->(g:GlobalEnrichment),
+      (p:Phenotype)-[:HAS_GLOBAL_ENRICHMENT]->(g),
+      (c:Ancestry)-[:HAS_GLOBAL_ENRICHMENT]->(g)
+where g.pValue < 5e-8 
+and r.annotation = 'Enhancer'
+and g.annotation = r.annotation 
+with v, r, t, p, c, head(apoc.coll.sortNodes(collect(g), "pValue")) as g
+
+// find the meta-analysis results for each of the phenotypes
+match (v)-[:HAS_META_ANALYSIS]->(m:MetaAnalysis),
+      (p)-[:HAS_META_ANALYSIS]->(m)
+where m.pValue < 1e-5
+
+return *
 
 
 
